19b3caf0b78042910b576f0a55066b1cce28a6a7
Cavidan Rəcəbli <recebli.cavidan@gmail.com>
2023-07-04 00:22:36 +0400

    Uptaded javascript

--- Sliding Signin Signup Form/script.js
+++ Sliding Signin Signup Form/script.js
@@ -9,3 +9,70 @@ signUpButton.addEventListener("click", () => {
 signInButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
 });
+document.getElementById("signUpForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!validateSignUpForm()) {
+    return;
+  }
+  // Formu gönder
+  e.target.submit();
+});
+
+document.getElementById("signInForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!validateSignInForm()) {
+    return;
+  }
+  // Formu gönder
+  e.target.submit();
+});
+
+function validateSignUpForm() {
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+
+  if (nameInput.value.trim() === "") {
+    showError("Name is required");
+    return false;
+  }
+  if (emailInput.value.trim() === "") {
+    showError("Email is required");
+    return false;
+  }
+  if (passwordInput.value.trim() === "") {
+    showError("Password is required");
+    return false;
+  }
+
+  return true;
+}
+
+function validateSignInForm() {
+  const signInEmailInput = document.getElementById("signInEmail");
+  const signInPasswordInput = document.getElementById("signInPassword");
+
+  if (signInEmailInput.value.trim() === "") {
+    showError("Email is required");
+    return false;
+  }
+  if (signInPasswordInput.value.trim() === "") {
+    showError("Password is required");
+    return false;
+  }
+
+  return true;
+}
+
+function showError(message) {
+  const errorDiv = document.createElement("div");
+  errorDiv.className = "error";
+  errorDiv.textContent = message;
+
+  const container = document.querySelector(".container");
+  container.insertBefore(errorDiv, container.firstChild);
+
+  setTimeout(() => {
+    errorDiv.remove();
+  }, 3000);
+}
